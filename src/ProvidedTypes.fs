656bb2e56e0974f6dd41e3860f4363145912cbda
TobyShaw <toby.shaw.96@gmail.com>
2018-01-18 11:31:02 +0000

    Fix for failing conversion of reflected types
    Unsure whether the assumption made is valid.

--- src/ProvidedTypes.fs
+++ src/ProvidedTypes.fs
@@ -8559,11 +8559,22 @@ namespace ProviderImplementation.ProvidedTypes
                      if canSave then table.[t] <- newT
                      newT
                 | _ ->
-                    let msg =
-                        if toTgt then sprintf "The design-time type '%O' utilized by a type provider was not found in the target reference assembly set '%A'. You may be referencing a profile which contains fewer types than those needed by the type provider you are using." t (getTargetAssemblies() |> Seq.toList)
-                        elif getSourceAssemblies() |> Seq.length = 0 then sprintf "A failure occured while determining compilation references"
-                        else sprintf "The target type '%O' utilized by a type provider was not found in the design-time assembly set '%A'. Please report this problem to the project site for the type provider." t (getSourceAssemblies() |> Seq.toList)
-                    failwith msg
+                    let asmsDomain = (if toTgt then getSourceAssemblies() else getTargetAssemblies())
+                    match tryFindInAssemblies asmsDomain with
+                    | Some (newT, canSave) ->
+                        let msg =
+                            if toTgt then 
+                                sprintf "The design-time type '%O' utilized by a type provider was not found in the target reference assembly set '%A'. You may be referencing a profile which contains fewer types than those needed by the type provider you are using." t (getTargetAssemblies() |> Seq.toList)
+                            elif getSourceAssemblies() |> Seq.length = 0 then
+                                sprintf "A failure occured while determining compilation references"
+                            else 
+                                sprintf "The target type '%O' utilized by a type provider was not found in the design-time assembly set '%A'. Please report this problem to the project site for the type provider." t (getSourceAssemblies() |> Seq.toList)
+                        failwith msg
+                    | None ->
+                        // Type does not exist in target or design-type assembly set.
+                        // Therefore it must be a reflected-type passed into a type provider
+                        // TODO: Check whether this assumption is valid.
+                        t
 
 
         and convType toTgt (t:Type) =
